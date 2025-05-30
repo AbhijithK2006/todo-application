10856f1bcd9ae3b93b0afda9f8663c93000527f8
Abhijith <abhijithkanamkotte@gmail.com>
2025-05-30 16:33:13 +0530

    ToDo App

---
+++ src/Nav.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+
+function Nav() {
+  return (
+    <>
+<nav className='bg-amber-200'>
+  <div class=" mx-auto p-4">
+    <div>
+      <ul class="font-medium flex md:space-x-8">
+        <li className='text-4xl'>
+          <a>ToDo</a>
+        </li>
+        <li className=''>
+          <a>HOME</a>
+        </li>
+        <li>
+          <a >ToDo</a>
+        </li>
+        
+      </ul>
+    </div>
+  </div>
+</nav>
+    </>
+  )
+}
+
+export default Nav
